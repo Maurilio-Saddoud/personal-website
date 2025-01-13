d2b07cc07b87a6a2c49d34c5f9d4cae6b8669b18
Maurilio Saddoud <masaddoud@davidson.edu>
2025-01-13 13:12:50 -0500

    integrated hosted chatbot

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css"
 import { Pages } from "./components/pages/Pages"
 import { useEffect } from "react"
+import { BubbleChat } from 'flowise-embed-react'
+
 
 //npm install --save aos@next
 //aos
@@ -16,6 +18,10 @@ function App() {
   return (
     <>
       <Pages />
+      <BubbleChat
+            chatflowid="11999498-2d92-4a14-9baa-54db37b1eb14"
+            apiHost="https://flowise-1buf.onrender.com"
+        />
     </>
   )
 }
